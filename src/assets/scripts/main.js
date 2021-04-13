5464c3774a4dd5e9c0a7254cb8c0462cf16c5df8
Dmitry <libafer99@gmail.com>
2021-04-14 00:05:30 +0500

    2 dim of last popup + minor fixes

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,7 +6,7 @@ import { App } from '../../modules/scripts/_core'
 document.addEventListener(`DOMContentLoaded`, function () {
     const app = new App()
     app.init()
-    
+    // UIkit.modal('#bani-item1').show()
     if (window.innerWidth < app.lg) {
         UIkit.slider('.s4__slider', {
             center: true
@@ -45,10 +45,14 @@ document.addEventListener(`DOMContentLoaded`, function () {
             }
         })
     })
+    UIkit.scrollspy(`.s3__list`)
+    document.querySelector(`.s3__list`).addEventListener(`inview`, (event) => {
+        document.querySelector(`.action-btn`).classList.add('visible')
+    })
     //VIDEO
     UIkit.scrollspy(`#video`)
     document.querySelector(`#video`).addEventListener(`inview`, (event) => {
-        document.querySelector(`#video .popup__body`).insertAdjacentHTML(`beforeend`, `<iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/0I7rYotTip4" frameborder="0" allowfullscreen="true" data-uk-video data-uk-responsive"></iframe>`)
+        document.querySelector(`#video .popup__body`).insertAdjacentHTML(`beforeend`, `<iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/fmT2FFVuWDA" frameborder="0" allowfullscreen="true" data-uk-video data-uk-responsive"></iframe>`)
     })
     // MAP SECTION
     UIkit.scrollspy(`.s10__map--1`)
