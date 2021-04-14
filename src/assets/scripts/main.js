a27d9b3c24bf9ceabab66bf7150c1346a5219779
Dmitry <libafer99@gmail.com>
2021-04-14 17:24:31 +0500

    all popups done

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,7 +6,6 @@ import { App } from '../../modules/scripts/_core'
 document.addEventListener(`DOMContentLoaded`, function () {
     const app = new App()
     app.init()
-    // UIkit.modal('#bani-item1').show()
     if (window.innerWidth < app.lg) {
         UIkit.slider('.s4__slider', {
             center: true
@@ -50,10 +49,22 @@ document.addEventListener(`DOMContentLoaded`, function () {
         document.querySelector(`.action-btn`).classList.add('visible')
     })
     //VIDEO
-    UIkit.scrollspy(`#video`)
-    document.querySelector(`#video`).addEventListener(`inview`, (event) => {
-        document.querySelector(`#video .popup__body`).insertAdjacentHTML(`beforeend`, `<iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/fmT2FFVuWDA" frameborder="0" allowfullscreen="true" data-uk-video data-uk-responsive"></iframe>`)
-    })
+    const setOfYTVideoIDs = [
+        'fmT2FFVuWDA',
+        'q_GlJi-F0ZM',
+        'SJAwaRU0BEw',
+        'ADAVVE7vxKk',
+        'J0y6KeYsprg',
+        'l4uX6SMFwp4',
+        'GVBiC5dEvf8',
+    ]
+    for (let i = 0; i < 7; i++) {
+        const videoId = `#video${i || ''}`
+        UIkit.scrollspy(videoId)
+        document.querySelector(videoId).addEventListener(`inview`, (event) => {
+            document.querySelector(`${videoId} .popup__body`).insertAdjacentHTML(`beforeend`, `<iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/${setOfYTVideoIDs[i]}" frameborder="0" allowfullscreen="true" data-uk-video data-uk-responsive"></iframe>`)
+        })
+    }
     // MAP SECTION
     UIkit.scrollspy(`.s10__map--1`)
     document.querySelector(`.s10__map--1`).addEventListener(`inview`, (event) => {
@@ -67,4 +78,23 @@ document.addEventListener(`DOMContentLoaded`, function () {
     document.querySelector(`.s10__map--3`).addEventListener(`inview`, (event) => {
         document.querySelector(`.s10__map--3 .s10__map-script`).insertAdjacentHTML(`beforeend`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2477.2459737047243!2d45.96897621602888!3d51.618703210379536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4114cf0d897828f9%3A0x69d3e661724b304d!2z0JLQvtC70YzRgdC60LjQuSDRgtGA0LDQutGCLCAy0JAsINCh0LDRgNCw0YLQvtCyLCDQodCw0YDQsNGC0L7QstGB0LrQsNGPINC-0LHQuy4sIDQxMDA2OQ!5e0!3m2!1sru!2sru!4v1618227948724!5m2!1sru!2sru" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`)
     })
+
+
+    const slidegal = (popup) => {
+        document.querySelectorAll(`${popup} .slidegal-item`).forEach((el, unused, parent) => {
+            el.addEventListener(`click`, (event) => {
+                UIkit.slider(`${popup} .banya__slider`).show(app.getIndexOfElements(event.currentTarget, parent))
+            })
+        })
+        app.sliderSpy(`${popup} .banya__slider`, index => {
+            app.changeActivitySet(document.querySelectorAll(`${popup} .slidegal-item`), index)
+        })
+    }
+    
+    slidegal('#bani-item1')
+    slidegal('#bani-item2')
+    slidegal('#bani-item3')
+    slidegal('#bani-item4')
+    slidegal('#bani-item5')
+
 })
